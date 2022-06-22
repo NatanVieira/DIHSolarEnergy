17a70bff0bb09067168a7a5f3f842b8d541384f2
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-22 16:17:32 -0300

    Config para Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+//Install express server
+const express = require('express');
+const path    = require('path');
+
+const app = express();
+
+//Server only the static files form the dist directory
+app.use(express.static('./dist/dihsolar-energy'));
+app.get('/*',(req, res) => res.sendFile('index.html', {root: 'dist/dihsolar-energy/'}));
+
+//Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
