6fa124ab47592770c0e53d192ea4edf0640a9dc0
Natanael Vieira <natanaelvieira130@gmail.com>
2022-06-22 16:45:45 -0300

    Config para Heroku

--- src/app/services/sessao-local.service.ts
+++ src/app/services/sessao-local.service.ts
@@ -10,7 +10,7 @@ export class SessaoLocalService {
   bgColor: string = '#EC6200';
   fgColor: string = 'black';
   cadastroAtualizacao: number = 0; // 0 não faz nada, 1 Sucesso, 2 Falha
-  URL_API_REST: string = 'http://localhost:3000/';
+  URL_API_REST: string = 'https://dihsolarenergy.herokuapp.com:3000/';
   
   constructor() { }
 
